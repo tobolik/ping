c95457988e5994567581354d2be1a65d38a127c6
tobolik <tobolik@users.noreply.github.com>
2025-10-03 13:59:03 +0200

    Refactor: Restructure backend file layout

--- api.php
+++ api.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$config = require 'config.php';
+$config = require 'config/config.php';
 
 // Handle preflight OPTIONS request for CORS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
